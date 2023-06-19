df0f44280b848a42f2dd7319ce4ed45eeddd8e0d
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-19 14:15:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
+	var table= document.getElementById("sampleTable");
+	var row=table.insertRow(0);
+
+	var cell1=row.insertCell(0);
+	var cell2=row.insertCell(1);
+	cell1.innerText="New Cell1";
+	cell2.innerText="New Cell2"
   
 }
